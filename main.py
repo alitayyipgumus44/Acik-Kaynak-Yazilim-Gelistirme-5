18b5d89db02e29c980fd662858a64a7d5150d6b7
Oguzhan Cabuk <91920210+oguzhancbk@users.noreply.github.com>
2022-12-21 21:21:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ def kare(k):
 
 def dikdortgen(k, u):
     print("Dikdörtgenin alanı = {}".format(k * u))
+    
+def daire(pi,r):
+    print("Dairenin alanı = {}".format((pi)*(r^2)))
 
 if __name__ == '__main__':
     print("""
     1 - Kare
     2 - Dikdörtgen
+    3 - Daire
     """)
 
     secim = int(input("Alanını hesaplamak istediğiniz şekil: "))
@@ -21,6 +25,10 @@ if __name__ == '__main__':
         k = int(input("Dikdörtgenin kısa kenarı: "))
         u = int(input("Dikdörtgenin uzun kenarı: "))
         dikdortgen(k, u)
+    elif secim == 3:
+        pi = 3.14
+        r = float(input("Yarı çap giriniz: "))
+        daire(pi,r)
 
     else:
         print("Sadece belirtilen sayılardan birini giriniz.")
